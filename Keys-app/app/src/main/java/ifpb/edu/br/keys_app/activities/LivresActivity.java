0123bb0672827dd1814f9c75a5fc64bc90ac6b26
Mateus de Lima <mateus.lima482@gmail.com>
2017-03-30 21:04:17 -0300

    Ajustes de layout

---
+++ Keys-app/app/src/main/java/ifpb/edu/br/keys_app/activities/LivresActivity.java
@@ -0,0 +1,22 @@
+package ifpb.edu.br.keys_app.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import ifpb.edu.br.keys_app.R;
+
+public class LivresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_livres);
+
+
+    }
+
+}
--- Keys-app/app/src/main/java/ifpb/edu/br/keys_app/activities/MainActivity.java
+++ Keys-app/app/src/main/java/ifpb/edu/br/keys_app/activities/MainActivity.java
@@ -1,19 +1,59 @@
 package ifpb.edu.br.keys_app.activities;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TabHost;
 
 import ifpb.edu.br.keys_app.R;
 
+import static ifpb.edu.br.keys_app.R.layout.activity_main;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    Button bt_locais_ocupados, bt_locais_livres, bt_locais;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_main);
+
+        bt_locais = (Button) findViewById(R.id.bt_locais);
+        bt_locais_ocupados = (Button) findViewById(R.id.bt_locais_ocupados);
+        bt_locais_livres = (Button) findViewById(R.id.bt_locais_livres);
+
+        bt_locais_ocupados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OcupadasActivity.class);
+                startActivity(intent);
+                finish();
+
+                }
+            });
+
+        bt_locais_livres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LivresActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        bt_locais .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LocaisActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
 
     }
 }
